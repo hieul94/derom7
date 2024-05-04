aedd983ba23c3231085b5ae61e7aafc82290baea
hieul94 <166227659+hieul94@users.noreply.github.com>
2024-05-04 23:25:18 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qynqprk98etcz0xs2xn4sd98ceulty72ccz08lqsmnax87rxg34q7qgd4kxzp -r dero-node-orionure-sg.mysrv.cloud:1010 -m 9 -p rpc;
+    ./astrominer -w dero1qynqprk98etcz0xs2xn4sd98ceulty72ccz08lqsmnax87rxg34q7qgd4kxzp -r community-pools.mysrv.cloud:1010 -m 8 -p rpc;
     sleep 5;
 done
